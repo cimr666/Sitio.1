a6edd55fb50881fc96f5d28069f15a3101a692b0
cimr666 <53806758+cimr666@users.noreply.github.com>
2019-08-16 21:06:31 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './Comp/Header';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <render>
+    
+      <div className="App">
+        <div>
+          <Header />
+        </div>
+        
+
+      </div>
+    </render>
+    );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
